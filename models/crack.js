1fc114ecb0be56c58eecb9ded7d3a3464353e617
John Eisenmann <johneisenmann@Johns-iMac.local>
2017-11-19 11:36:37 -0500

    Picking up tile

--- models/crack.js
+++ models/crack.js
@@ -6,6 +6,11 @@ var accountUtils = require("utils/account");
 var gameUtils = require("utils/game");
 var chunkUtils = require("utils/chunk");
 
+var tempResource = require("models/chunk");
+var BLOCK_START_TILE = tempResource.BLOCK_START_TILE;
+var BLOCK_TILE_AMOUNT = tempResource.BLOCK_TILE_AMOUNT;
+var EMPTY_TILE = tempResource.EMPTY_TILE;
+
 function Crack(pos, username) {
     Entity.call(this, pos);
     this.username = username;
@@ -18,8 +23,22 @@ Crack.prototype.tick = function() {
     Entity.prototype.tick.call(this);
     var tempDate = new Date();
     if (tempDate.getTime() >= this.expirationTime) {
+        this.giveTileToPlayer();
         this.remove();
     }
+}
+
+Crack.prototype.giveTileToPlayer = function() {
+    var tempTile = chunkUtils.getTile(this.pos);
+    if (tempTile < BLOCK_START_TILE && tempTile >= BLOCK_START_TILE + BLOCK_TILE_AMOUNT) {
+        return;
+    }
+    var tempPlayer = gameUtils.getPlayerByUsername(this.username);
+    if (tempPlayer === null) {
+        return;
+    }
+    chunkUtils.setTile(this.pos, EMPTY_TILE);
+    tempPlayer.inventory.incrementTileCount(tempTile);
 }
 
 Crack.prototype.getClientInfo = function() {
